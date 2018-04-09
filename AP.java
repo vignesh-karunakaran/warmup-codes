e6599be8f1311894bb24106910dbe04b01eb2797
vignesh karunakaran <vickyredwon@gmail.com>
2018-04-09 23:46:47 +0530

    Update AP.java

--- AP.java
+++ AP.java
@@ -4,19 +4,31 @@ public class AP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int no,st =1,dif;
+int no,st,dif;
 		
 	Scanner c= new Scanner(System.in);
-System.out.println("enter total count numbers");	
-no=	c.nextInt();
-System.out.println("enter the difference numbers");
-dif=c.nextInt();
-	int a[]=new int[no];
-for(int i=0;i<a.length;i++) {
+		
+	System.out.println("enter starting point");
+	
+		st = c.nextInt();
+	
+		System.out.println("enter total count numbers");	
+	
+		no=c.nextInt();
+	
+		System.out.println("enter the difference numbers");
+	
+		dif=c.nextInt();
+	
+	for(int i=0;i<no;i++) {
 
-	System.out.print(st+",");
-	st += dif;
-}
+	
+		System.out.print(st+",");
+	
+		st += dif;
+	}
+	
+	c.close();
 	
 	
 	}
