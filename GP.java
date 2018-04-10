ec6c691059956a48d3754f04e6ed1c2922946fe8
vignesh karunakaran <vickyredwon@gmail.com>
2018-04-10 17:47:31 +0530

    Update GP.java

--- GP.java
+++ GP.java
@@ -4,18 +4,31 @@ public class GP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	//starting point,common ratio, n terms to print
 		int st,cr,n;
+		 
 		Scanner sc=new Scanner(System.in);
-	    System.out.println("Enter starting term");
-	   st=sc.nextInt();
-	    System.out.println("enter common ratio");
-	    cr=sc.nextInt();
-	   System.out.println("enter n terms");
-	    n=sc.nextInt();
-	   for(int i=0;i<n;i++) {
-	    	   System.out.print((st)+",");
-	    	st*=cr;
+	    
+		System.out.println("Enter starting term");
+	  
+		st=sc.nextInt();
+	    
+		System.out.println("enter common ratio");
+	   
+		cr=sc.nextInt();
+	   
+		System.out.println("enter n terms");
+	   
+		n=sc.nextInt();
+	  
+		for(int i=0;i<n;i++) {
+	    	   
+			System.out.print((st)+",");
+	    	
+			st*=cr;
 	     
-	}
+	
+		}
+		sc.close();
 	}
 }
