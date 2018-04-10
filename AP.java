57168bf855da76ca67d2c1eea5ecb1247ecbe2fb
vignesh karunakaran <vickyredwon@gmail.com>
2018-04-10 17:42:58 +0530

    Update AP.java

--- AP.java
+++ AP.java
@@ -4,7 +4,9 @@ public class AP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int no,st,dif;
+//no is how manynumbers to print,st is starting point ,dif is difference.
+		
+		int no,st,dif;
 		
 	Scanner c= new Scanner(System.in);
 		
@@ -19,12 +21,12 @@ int no,st,dif;
 		System.out.println("enter the difference numbers");
 	
 		dif=c.nextInt();
-	
+//using for loop to print till how many numbers to print.	
 	for(int i=0;i<no;i++) {
 
-	
+	//printing the starting value before adding difference.
 		System.out.print(st+",");
-	
+	//difference is added to starting point till for loop condition exists
 		st += dif;
 	}
 	
