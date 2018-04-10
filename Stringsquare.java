cde21bbb6f83aef18076bc7fd234c8d075d01d60
vignesh karunakaran <vickyredwon@gmail.com>
2018-04-11 01:06:18 +0530

    Update Stringsquare.java

--- Stringsquare.java
+++ Stringsquare.java
@@ -16,27 +16,16 @@ for(int i=0;i<num;i++) {
 
 	
 	for(int j=0;j!=num;j++) {
-//k is assigned here 
-	k = i+j;
 
-		while(k<num){
-//if the k below num it will print the character for the assigned k value which is added with the ascii integer value from A-65
-	System.out.print((char)(65+k));
-	break;
+		//k is assigned here 
 	
-		}
-//if the k is greater than or equal to num this while loop is executed
-	while(k>=num)
-	{
-//k value is subtracted to num value eg:- if k=3 then(3-3) those k value is assigned zero,if k=4 then (4-3) then k is assigned to 1 and so on...
-	
-	k -=num;
-	
-	System.out.print((char)(65+k));
-	
-	}
-	
+		k = i+j;
 
+		//using conditional operator instead of using more while loops
+	
+		k =(k<num) ? k : k-num;
+	
+		System.out.print((char)(65+k));
 	
 	}
 
