dfabc6b1225e520fd2677901b2191403c5eae90f
vignesh karunakaran <vickyredwon@gmail.com>
2018-04-12 18:38:08 +0530

    Update Smalle.java

--- Smalle.java
+++ Smalle.java
@@ -7,7 +7,7 @@ public class Smalle {
 	//num is number ,l is off of the number 	
     
     int num,l,b;
-		    
+		String k="";    
         Scanner v= new Scanner(System.in);
 	
            num =v.nextInt();
@@ -22,8 +22,10 @@ public class Smalle {
                   b++;
 		
 			
-			System.out.print(b+",");
-		}
+			System.out.print(k+b);
+	 //initalizing k after first number printed.     
+	       k=",";
+       }
 		
 	}
 
