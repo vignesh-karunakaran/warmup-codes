2052cea2ff8124b78f2289dada789c2efcf04162
vignesh karunakaran <vickyredwon@gmail.com>
2018-05-28 21:02:03 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+class Triangle{
+
+
+public static void main(String v[]){
+
+int m;
+int k=1;
+for(int i=0;i<m;i++)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.pint(k);
+k++;
+}
+System.out.println();
+}
+
+}
+}
